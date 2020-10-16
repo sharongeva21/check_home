89577eab4ade92e8172ca4bbada1c2541af63d7a
sharongeva21 <58490251+sharongeva21@users.noreply.github.com>
2020-10-16 16:12:59 +0300

    Add files via upload

---
+++ dropdown.js
@@ -0,0 +1,127 @@
+// drop down for the house number
+function processData(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            var tarr = [];
+            for (var j=0; j<data.length; j++) {
+                //tarr.push(headers[j]+":"+data[j]);
+				data[j] = data[j].substring(1, data[j].length-1)
+
+				tarr.push(data[j]); // thake just the values without header
+            }
+            lines.push(tarr);
+        }
+    }
+     
+	x = lines
+	
+	// push each value in x to the dropdown
+const home_num = document.getElementById("readHomeNum")
+
+	x.forEach( x => {	const option = document.createElement("option")
+		option.textContent = x
+		//option.id = t.id
+		home_num.appendChild(option)
+		})
+}
+// use processData() and use x
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "homes.csv",
+        dataType: "text",
+        success: function(data) {processData(data);}
+     });
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// dropdown for first name
+function processData2(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            var tarr = [];
+            for (var j=0; j<data.length; j++) {
+                //tarr.push(headers[j]+":"+data[j]);
+				tarr.push(data[j]); // thake just the values without header
+            }
+            lines.push(tarr);
+        }
+    }
+     
+	x = lines
+	
+	// push each value in x to the dropdown
+const first_name = document.getElementById("readFirstName")
+
+	x.forEach( x => {	const option = document.createElement("option")
+		option.textContent = x
+		//option.id = t.id
+		first_name.appendChild(option)
+		})
+}
+// use processData() and use x
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "firts_name.csv",
+        dataType: "text",
+        success: function(data) {processData2(data);}
+     });
+});
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// dropdown for second name
+function processData3(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            var tarr = [];
+            for (var j=0; j<data.length; j++) {
+                //tarr.push(headers[j]+":"+data[j]);
+				tarr.push(data[j]); // thake just the values without header
+            }
+            lines.push(tarr);
+        }
+    }
+     
+	x = lines
+	
+	// push each value in x to the dropdown
+const second_name = document.getElementById("readSecondName")
+
+	x.forEach( x => {	const option = document.createElement("option")
+		option.textContent = x
+		//option.id = t.id
+		second_name.appendChild(option)
+		})
+}
+// use processData() and use x
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "second_name.csv",
+        dataType: "text",
+        success: function(data) {processData3(data);}
+     });
+});
+
+
+
